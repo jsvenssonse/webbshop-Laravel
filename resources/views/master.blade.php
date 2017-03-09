d9524043c69c5cdafec32ae867ea8e35bb02ed8c
Jesper Svensson <j.svensson.se@gmail.com>
2017-03-09 11:37:34 +0100

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+
+        {!! Html::script('libs/js/jquery-3.1.1.min.js') !!}
+        {!! Html::script('libs/js/bootstrap.min.js') !!}
+
+        {!! Html::style('libs/css/bootstrap.min.css.css') !!}
+
+        <title>Webbshop</title>
+    </head>
+
+
+        <div class="container">
+
+            @yield('content')
+
+        </div>
+
+
+    </body>
+
+
+</html>
